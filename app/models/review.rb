8f1ebdb06abe6356f57d3bb6c6f2e7b887c43e9f
Maximilian Mai <maximilian@mai-mail.com>
2024-08-21 18:23:06 +0800

    code restructure

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-  CATEGORIES = (0..5)
+  RATING = (0..5)
   belongs_to :restaurant
   validates :content, :rating, presence: true
-  validates :rating, numericality: { only_integer: true, in: Review::CATEGORIES }
+  validates :rating, numericality: { only_integer: true, in: RATING }
 end
